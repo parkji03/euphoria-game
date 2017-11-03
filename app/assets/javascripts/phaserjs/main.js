86118b427b70c0a0c63d756798ba9fc285163a90
jihoonpark4829@gmail.com <jihoonpark4829@gmail.com>
2017-11-02 22:50:10 -0600

    game structure

--- app/assets/javascripts/phaserjs/main.js
+++ app/assets/javascripts/phaserjs/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1200, 600, Phaser.AUTO, 'game-container', { preload: preload, create: create, update: update });
 
 function preload() {
 
