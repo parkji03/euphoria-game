8b5e7e848daf601fba496fe306670f7270674d79
jihoonpark4829@gmail.com <jihoonpark4829@gmail.com>
2017-11-24 20:57:19 -0700

    fix

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module EuphoriaGame
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     config.before_configuration do
